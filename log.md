cd4a0d1d2f2a3f3f0a92ec4d964e0ae5abab0b12
Orc <672497611@qq.com>
2026-02-14 23:37:38 +0800

    a

--- log.md
+++ log.md
@@ -12,3 +12,17 @@
     飞行物做成肥皂泡和各种小动物，有不同的碰撞体积和飞行速度。
     可以被子弹击中得分，时长最短的得分更高。 得分排名存储。
 
+-----安卓编译---------
+1、安装android SDK, NDK
+2、配置环境变量
+rustup target add aarch64-linux-android
+rustup target add armv7-linux-androideabi
+cargo install cargo-ndk
+
+
+--windows编译----
+cargo run --features desktop
+
+---android 项目 [armeabi-v7a, arm64-v8a]
+cargo ndk -t arm64-v8a -o android/app/src/main/jniLibs build --release
+cargo ndk -t armeabi-v7a -o android/app/src/main/jniLibs build --release
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,83 @@
-use bevy::log::LogPlugin;
-use bevy::prelude::*;
-use bevy::window::PrimaryWindow;
-use crate::boids_plug::*;
-use crate::cannon_plug::*;
-use crate::bullet_plug::*;
-use crate::config::*;
-use crate::score_plug::*;
-use crate::fps_plug::*;
-use crate::game_state_plug::*;
-use crate::menu_plug::*;
-use crate::cutscene_plug::*;
+use Fly_Strike::main;
 
-mod boids_plug;
-mod config;
-mod cannon_plug;
-mod event;
-mod bullet_plug;
-mod score_plug;
-mod fps_plug;
-mod game_state_plug;
-mod menu_plug;
-mod cutscene_plug;
+// use bevy::log::LogPlugin;
+// use bevy::prelude::*;
+// use bevy::window::PrimaryWindow;
+// use crate::boids_plug::*;
+// use crate::cannon_plug::*;
+// use crate::bullet_plug::*;
+// use crate::config::*;
+// use crate::score_plug::*;
+// use crate::fps_plug::*;
+// use crate::game_state_plug::*;
+// use crate::menu_plug::*;
+// use crate::cutscene_plug::*;
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(GameStatePlug)
-        .add_plugins(MenuPlug)
-        .add_plugins(CannonPlugin)
-        .add_plugins(BoidsPlugin)
-        .add_plugins(BulletPlugin)
-        .add_plugins(ScorelPlugin)
-        .add_plugins(FpsPlugin)
-        .add_systems(PreStartup, pre_startup)
-        .add_systems(Startup, setup)
-        .run();
-}
+// mod boids_plug;
+// mod config;
+// mod cannon_plug;
+// mod event;
+// mod bullet_plug;
+// mod score_plug;
+// mod fps_plug;
+// mod game_state_plug;
+// mod menu_plug;
+// mod cutscene_plug;
 
-fn pre_startup(
-    window: Single<&Window, With<PrimaryWindow>>,
-    mut commands: Commands, 
-    asset_server: Res<AssetServer>
-) {
-    commands.spawn(Camera2d);
-    load_font(&mut commands, &asset_server);
+// fn main() {
+//     Fly_Strike::run();
+// }
 
-    //获取窗口宽高
-    let width = window.width();
-    let height = window.height();
-    commands.insert_resource(GameConfig::default().width_window_size(width, height));
+// fn main() {
+//     App::new()
+//         .add_plugins(DefaultPlugins)
+//         .add_plugins(GameStatePlug)
+//         .add_plugins(MenuPlug)
+//         .add_plugins(CannonPlugin)
+//         .add_plugins(BoidsPlugin)
+//         .add_plugins(BulletPlugin)
+//         .add_plugins(ScorelPlugin)
+//         .add_plugins(FpsPlugin)
+//         .add_systems(PreStartup, pre_startup)
+//         .add_systems(Startup, setup)
+//         .run();
+// }
+
+// fn pre_startup(
+//     window: Single<&Window, With<PrimaryWindow>>,
+//     mut commands: Commands, 
+//     asset_server: Res<AssetServer>
+// ) {
+//     commands.spawn(Camera2d);
+//     load_font(&mut commands, &asset_server);
+
+//     //获取窗口宽高
+//     let width = window.width();
+//     let height = window.height();
+//     commands.insert_resource(GameConfig::default().width_window_size(width, height));
     
-    //音效
-    let sound = asset_server.load("sounds/score.wav");
-    commands.insert_resource(ScoreSound(sound.clone()));
+//     //音效
+//     let sound = asset_server.load("sounds/score.wav");
+//     commands.insert_resource(ScoreSound(sound.clone()));
 
-    let sound = asset_server.load("sounds/fireworks.wav");
-    commands.insert_resource(FireworksSound(sound.clone()));
+//     let sound = asset_server.load("sounds/fireworks.wav");
+//     commands.insert_resource(FireworksSound(sound.clone()));
 
-    let sound = asset_server.load("sounds/game_started.wav");
-    commands.insert_resource(GameStartedSound(sound.clone()));
+//     let sound = asset_server.load("sounds/game_started.wav");
+//     commands.insert_resource(GameStartedSound(sound.clone()));
 
-}
+// }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut game_state: ResMut<NextState<GameState>>
-) {
-    game_state.set(GameState::BeforeInGame);
-}
+// fn setup(
+//     mut commands: Commands,
+//     asset_server: Res<AssetServer>,
+//     mut game_state: ResMut<NextState<GameState>>
+// ) {
+//     game_state.set(GameState::BeforeInGame);
+// }
 
 
-fn load_font(commands: &mut Commands, asset_server: &Res<AssetServer>) {
-    let handle = asset_server.load("fonts/default_zh.ttf");
-    commands.insert_resource(GlobalFont(handle));
-}
+// fn load_font(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+//     let handle = asset_server.load("fonts/default_zh.ttf");
+//     commands.insert_resource(GlobalFont(handle));
+// }
